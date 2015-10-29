551264357b44c62d0dabc9749ca53600c7c8acd1
williamrenwick <william.r.renwick@gmail.com>
2015-10-29 16:48:36 +0000

    updating production config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,28 +1,43 @@
-var Webpack = require('webpack');
+var webpack = require('webpack');
 var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
 var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 var buildPath = path.resolve(__dirname, 'public', 'build');
 var mainPath = path.resolve(__dirname, 'app', 'main.js');
 
-var config = {
-
-  // We change to normal source mapping
+module.exports = {
   devtool: 'source-map',
-  entry: mainPath,
+  entry: [ 
+    mainPath
+  ],
   output: {
     path: buildPath,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: [nodeModulesPath]
-    },{
-      test: /\.css$/,
-      loader: 'style!css'
-    }]
-  }
+    loaders: [
+      { 
+        test: /\.js$/, 
+        loader: 'babel',
+        exclude: [nodeModulesPath],
+      }, {
+        test: /\.css$/,
+        loader: 'style!css'
+      }, {
+        test: /\.(png|jpg)$/,
+        loader: 'url?limit=25000'
+      }, {
+        test: /\.woff$/,
+        loader: 'url?limit=100000'
+      }
+    ]
+  },
+  plugins: [
+      new webpack.ProvidePlugin({
+        $ : "jquery",
+        jQuery : "jquery",
+        "window.jQuery" : "jquery",
+        "root.jQuery" : "jquery"
+      })
+  ]
 };
-
-module.exports = config;
