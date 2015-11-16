9f7ccd0d0d003899d2f8b014ce28baeaaf8688ca
williamrenwick <william.r.renwick@gmail.com>
2015-11-16 10:32:20 +0000

    new css preload

--- app/components/AppContents.react.js
+++ app/components/AppContents.react.js
@@ -8,6 +8,7 @@ var mixin = require('baobab-react/mixins').branch;
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
 var LoadingActions = require('../actions/LoadingActions.js')
+var photosForPreload = require('../data/photoList.js').allPhotoUrls;
 
 
 var AppContents = React.createClass({
@@ -17,11 +18,21 @@ var AppContents = React.createClass({
 		isPreloaded: ['loading', 'isPreloaded']
 	},
 
+	renderImagePreloader: function() {
+		return photosForPreload.map((url, i) => { 
+            return (
+               <img src={url} id={i} key={i}/>
+            ) 
+        })
+	},
 	render: function() {
 		if (!this.state.isPreloaded) {
 			return (
 				<div id="reactWrap">
 					<Loader />
+					<div id='preload'>
+						{this.renderImagePreloader()}
+					</div>
 				</div>
 			)
 		} else {
